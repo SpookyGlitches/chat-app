8210d05720f08da7378190ccf1bc730266df9021
SpookyGlitches <earljseph@gmail.com>
2022-02-28 00:20:45 +0800

    remove layout

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,7 @@ import "../styles/globals.css";
 import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }) {
-	const getLayout = Component.getLayout || ((page) => page);
-	return getLayout(<Component {...pageProps} />);
+	return <Component {...pageProps} />
 }
 
 export default MyApp;
